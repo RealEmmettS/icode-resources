9bf2bfdb80d5c67b0e43bdbc83d0bae82f109c0e
Emmett Shaughnessy <github@emmetts.dev>
2023-09-27 16:48:13 -0500

    removed Copilot prompts

--- accessories/script.js
+++ accessories/script.js
@@ -1,24 +1,3 @@
-// create a basic website, styled with CSS.
-// Include a title, and one button that says
-// "Download Resource Zip". When the button is
-// clicked, the user should be prompted for a password (iCode002).
-//
-// If the password is correct, the user should be redirected to an
-// AWS S3 link that will start the download.
-//
-// If the password is incorrect, the user should be alerted that
-// the password is incorrect.
-//
-// The AWS link hasn't been created yet, but it will be similar to:
-// https://icode-files.s3.amazonaws.com/resources2023.zip
-//
-//The HTML <body> Code is:
-// <div class="container">
-//     <h1>Download Resource Zip</h1>
-//     <button id="download">Download Resource Zip</button>
-// </div>
-// <script src="js/script.js"></script>
-
 function downloadZip() {
     var password = prompt("Enter Password");
     if (password === "iCode002") {
